f918d50711694f02eef70451c3f695fd488039fb
e-calderonm <166415423+e-calderonm@users.noreply.github.com>
2025-11-28 12:24:18 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- ¡IMPORTANTE: Agrega esta línea arriba!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Si estamos en producción (Vercel), forzamos HTTPS
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
